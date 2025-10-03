e88e585639c5e5c43b85f3e8527ac6cb18386bc4
inferno0230 <mail@inferno0230.in>
2025-10-03 10:11:58 +0530

    caihong: Initial pull from OPD2403_15.0.0.1301(EX01)
    Signed-off-by: inferno0230 <mail@inferno0230.in>

---
+++ Android.mk
@@ -0,0 +1,34 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),caihong)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,d8baf8a131bbda1675658b96a74c46a9a4ac817f)
+$(call add-radio-file-sha1-checked,radio/aop.img,315d8990850f92cbcc0dd06453ce56589832282b)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,30a5c35f75af33239bfe25249fb00945d91e552f)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,5d3ffa25a4317bc33d2c83b955b5e09669dd210c)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,6ab303d33ecc33ba1259e661641d2fc47112ab51)
+$(call add-radio-file-sha1-checked,radio/cpucp_dtb.img,ef2af5d358e748fd6d6c9bd14f730a936831cbd4)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,e1c58cb7e8fdc10c56d3f1326af46a1e38b58f6f)
+$(call add-radio-file-sha1-checked,radio/dsp.img,72075bb397712d2b7753e2a8654cf8e5c2313602)
+$(call add-radio-file-sha1-checked,radio/engineering_cdt.img,13f5f33df779408b04cdfb3d78dd1ebbd3afb7b4)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,aebbe7cd5ee7205cc912d14485afabebf99b723e)
+$(call add-radio-file-sha1-checked,radio/hyp.img,41da3b9c739f88830d92ff16131a58d405a2b34e)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,0bbc34e563138ef67daa478b3c867c7eec45c7af)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,ec12c9170282c0c5d2d76dfa23521463be982a0a)
+$(call add-radio-file-sha1-checked,radio/modem.img,ba01f00f9d51fbee79a758ae7b480a888b47bc58)
+$(call add-radio-file-sha1-checked,radio/oplus_sec.img,2a0a3bc41fa77874e80515aaee0a151fd7ea2756)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,0df0fe98702448fa346b891cd7f4a973c8a9d4ef)
+$(call add-radio-file-sha1-checked,radio/shrm.img,cea21865fc73f431facae50f1595abefda2eaee7)
+$(call add-radio-file-sha1-checked,radio/splash.img,2cfaecc05c210fbb7546994816ab28ca358e97cb)
+$(call add-radio-file-sha1-checked,radio/tz.img,7a68b53ee2ddf90987c39c86b6d8bf7f484c2554)
+$(call add-radio-file-sha1-checked,radio/uefi.img,e16d294f2c35cc6d37a24e3f7fde7c653038ef43)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,29acdc2a9d4cc909aacb368e3c7aa8e3b22b2931)
+$(call add-radio-file-sha1-checked,radio/xbl.img,8a00f11f1819128e441bbf591b1f09fb670d6fbb)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,7ab5b990662ee10f5a4e860e5e766bfc1fe14e07)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,fdf8885cf27c30932d7ec99dae89a919c10083ad)
+
+endif
